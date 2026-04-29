f3f92b5c93418755bc99e5bc8ee5c928751ea1d5
gulsum-mehdiyeva <mehdiyevaglsm@gmail.com>
2026-04-29 21:54:28 +0400

    Join ile islemler yapildi

--- src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -9,12 +9,21 @@ import java.util.List;
 public interface KitapRepository extends JpaRepository<Kitap, Long> {
 
     //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
-    String QUESTION_1 = "";
+    String QUESTION_1 =
+            "SELECT k.kitapno, k.ad, k.puan, k.yazarno, k.turno " +
+                    "FROM kitap k " +
+                    "WHERE k.turno IN ( " +
+                    "SELECT t.turno " +
+                    "FROM tur t " +
+                    "WHERE t.ad IN ('Dram', 'Hikaye') " +
+                    ")";
+
     @Query(value = QUESTION_1, nativeQuery = true)
     List<Kitap> findBooks();
 
 
-    String QUESTION_10 = "";
+    String QUESTION_10 = "SELECT AVG(k.puan)" +
+            " FROM kitap AS k";
     @Query(value = QUESTION_10, nativeQuery = true)
     Double findAvgPointOfBooks();
 
